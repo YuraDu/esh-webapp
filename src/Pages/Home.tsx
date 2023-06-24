3dd35c6248e633e86c7cc288e4d8f9a45274058c
yuradukhno <yura.dukhno@gmail.com>
2023-06-24 15:01:29 +0300

    little animation added in the main route

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -2,15 +2,33 @@ import React, { useEffect, useState } from "react";
 import "./Home.css";
 
 const Home: React.FC = () => {
+  const [isHovered, setIsHovered] = useState(false);
   const [isRendered, setIsRendered] = useState(false);
 
   useEffect(() => {
     setIsRendered(true);
   }, []);
 
+  const handleHover = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
   return (
     <div className="home-container">
-      <h1 className={`animation-text ${isRendered ? "animate" : ""}`}>esh.</h1>
+      <h1
+        className={`animation-text ${isRendered ? "animate" : ""}`}
+        onMouseEnter={handleHover}
+        onMouseLeave={handleMouseLeave}
+      >
+        esh.
+        <div className={`hovered-text ${isHovered ? "show" : ""}`}>
+          A new bank is coming
+        </div>
+      </h1>
     </div>
   );
 };
