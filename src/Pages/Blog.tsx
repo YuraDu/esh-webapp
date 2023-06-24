2c5b6b36574a3537f646951377116ea70a29acb7
yuradukhno <yura.dukhno@gmail.com>
2023-06-24 14:04:29 +0300

    comment added

--- src/Pages/Blog.tsx
+++ src/Pages/Blog.tsx
@@ -5,6 +5,23 @@ import { Post } from "./../Components/Post";
 import "./Blog.css";
 
 const Blog: React.FC = () => {
+  // Use this section if fetching blog data from an API
+  // ===============================================
+  // const [blogData, setBlogData] = useState([]);
+
+  // useEffect(() => {
+  //   // Fetch blog data using Axios
+  //   axios
+  //     .get("your-api-endpoint")
+  //     .then(response => {
+  //       setBlogData(response.data);
+  //     })
+  //     .catch(error => {
+  //       console.error("Error fetching blog data:", error);
+  //     });
+  // }, []);
+  // ===============================================
+
   return (
     <div className="blog-container">
       {blogData.posts.map((post, index) => (
